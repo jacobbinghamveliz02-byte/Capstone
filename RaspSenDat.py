99199fcbf69eb2944046dd7d7d425b32f60ba40d
Jacob <jacobbinghamveliz02@gmail.com>
2026-01-14 19:39:09 -0700

    Prints sensor data test 2

--- RaspSenDat.py
+++ RaspSenDat.py
@@ -5,6 +5,5 @@ serialPort = serial.Serial("/dev/ttyACM1", 9600) #Open the serial port
 serialPort.reset_input_buffer() #Clears inputs before starting
 
 while True:
-    if serialPort.in_waiting > 0: #checking for data
-        data = serialPort.readline().decode('utf-8').rstrip() #read the data
-        print(data) #print the data to the console  
+    data = serialPort.readline().decode('utf-8').rstrip() #read the data
+    print(data) #print the data to the console  
