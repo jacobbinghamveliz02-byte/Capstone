a5028a31fc4a9a3b6031f58f63e6ef7ec344756f
jacobbinghamveliz02-byte <jacobbinghamveliz02@gmail.com>
2026-01-14 19:48:03 -0700

    Add files via upload

---
+++ SensorCode/SensorCode.ino
@@ -0,0 +1,21 @@
+int light_sensor = A3;
+int temp_sensor = A2;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int raw_light = analogRead(light_sensor);
+  int light = map(raw_light, 0, 1023, 0, 100);
+  int raw_temp = analogRead(temp_sensor);  
+  float voltage = raw_temp * (5.0 / 1023.0);
+  float fahrenheit = ((voltage * 10) * 9.0 / 5.0) + 32.0;
+
+  Serial.print("Temperature: ");
+  Serial.print(fahrenheit);
+  Serial.println("°F)");
+  Serial.print("Light leve: ");
+  Serial.println(light);
+  delay(1000);
+}
