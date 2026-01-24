18f363bfd9331e8b3b5decb308f7bc28f46ac817
Jacob <jacobbinghamveliz02@gmail.com>
2026-01-23 17:52:00 -0700

    testing light control function and removed redundant testTargetValue call.

--- Raspberrypi/com.js
+++ Raspberrypi/com.js
@@ -99,8 +99,8 @@ async function main() {
     // console.log(allValueIds);
     // fs.writeFileSync(`${currentDir}/light2.json`, JSON.stringify(allValueIds, null, 2));
     // thermostat();
-    // lightControl();
-    testTargetValue();
+    lightControl();
+    // testTargetValue();
 }
 
 async function thermostat() {
@@ -188,11 +188,6 @@ async function diagnoseNode4() {
         }
     }, 2000);
 }
-
-await testTargetValue();
-
-
-
 
 
 // Start the driver
