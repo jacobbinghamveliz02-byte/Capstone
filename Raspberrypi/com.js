3ba26b9aca784bf964cf8b4e65b784588e6e9da1
Jacob <jacobbinghamveliz02@gmail.com>
2026-02-11 21:58:07 -0700

    fixed the mqtt connection to use the correct host, port, protocol, username, and password. Also fixed the propertyKey in the ThermostatValueId.json file to be undefined instead of "undefined".

--- Raspberrypi/com.js
+++ Raspberrypi/com.js
@@ -25,6 +25,15 @@ const driver = new Driver(
     },
 );
 
+var options = {
+    host: "57cb3b2fa5314c20af5ed5e2001f4a4c.s1.eu.hivemq.cloud",
+    port: 8883,
+    protocol: "mqtts",
+    username: "Raspberry",
+    password: "RaspberrypiPassword1"
+    };
+client = mqtt.connect(options);
+
 // [5-67-0-setpoint-1] Setpoint (Heating) 
 
 const TEMPATURE = {
@@ -137,13 +146,7 @@ driver.once("driver ready", () => {
 
 
 async function main() {
-    var options = {
-        host: "57cb3b2fa5314c20af5ed5e2001f4a4c.s1.eu.hivemq.cloud",
-        port: 8883,
-        protocol: "mqtts",
-        username: "Raspberry",
-        password: "RaspberrypiPassword1"
-    };
+    
     thermostatNode = driver.controller.nodes.get(THERMID);
     lightNode1 = driver.controller.nodes.get(LIGHTID[0]);
 
